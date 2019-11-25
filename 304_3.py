c25d7d11aaec898d5fc55995cf64f4c75f71d65a
ATELIER30X <57906636+ATELIER30X@users.noreply.github.com>
2019-11-25 18:08:31 +0100

    Add files via upload

---
+++ 304_3.py
@@ -0,0 +1,13 @@
+def moje_funkce():
+    #radky musi byt odsazene
+    print ("print uvnitr funkce")
+
+
+def moje_druha_funkce(x, y):
+    return (x + y)
+    
+
+def multiples(n):
+    print("Dvojnasobek", n*2)
+    print("Trojnasobek", n*3)
+        
